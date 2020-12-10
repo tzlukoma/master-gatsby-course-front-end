35eb061481a2dcb3cb6deef22a6642a37144b168
Thomas Z Lukoma <tzlukoma@gmail.com>
2020-12-09 22:56:37 -0500

    feat: :tada: Initial commit for wes bos master gatsby course

---
+++ src/pages/404.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function FourOhFourPage() {
+  return (
+    <>
+      <p>Hey - that page doesn't exist</p>
+    </>
+  );
+}
---
+++ src/pages/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function HomePage() {
+  return (
+    <>
+      <p>Hey! I'm the EEEE home page</p>
+    </>
+  );
+}
