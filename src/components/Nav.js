0c7e68b32087ba4a616d4cce10a41f21cb856ac2
Thomas Z Lukoma <tzlukoma@gmail.com>
2020-12-10 00:47:40 -0500

    feat: :lipstick: Adds styles to layout

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -17,6 +17,7 @@ const NavStyles = styled.nav`
     grid-template-columns: 1fr 1fr auto 1fr 1fr;
     grid-gap: 2rem;
     align-items: center;
+    margin-top: -6rem;
   }
   li {
     --rotate: -2deg;
