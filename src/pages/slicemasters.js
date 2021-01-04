781d104cae610267667b5499974a863b189f84cc
Thomas Z Lukoma <tzlukoma@gmail.com>
2021-01-03 20:42:03 -0500

    Pagination complete

--- src/pages/slicemasters.js
+++ src/pages/slicemasters.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Link, graphql } from 'gatsby';
 import Img from 'gatsby-image';
 import styled from 'styled-components';
+import Pagination from '../components/Pagination';
 
 const SliceMasterGrid = styled.div`
   display: grid;
@@ -36,12 +37,19 @@ const SliceMasterStyles = styled.div`
   }
 `;
 
-export default function SlicemastersPage({ data }) {
+export default function SlicemastersPage({ data, pageContext }) {
   const slicemasters = data.slicemasters.nodes;
 
   console.log(data.slicemasters.totalCount);
   return (
     <>
+      <Pagination
+        pageSize={parseInt(process.env.GATSBY_PAGE_SIZE)}
+        totalCount={data.slicemasters.totalCount}
+        currentPage={pageContext.currentPage || 1}
+        skip={pageContext.skip}
+        base="slicemasters"
+      />
       <SliceMasterGrid>
         {slicemasters.map((person) => (
           <SliceMasterStyles key={person.id}>
